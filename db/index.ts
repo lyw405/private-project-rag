e2061a3856cbb5d190ba363ca6faf2e52349ef07
nafek.liu <nafek0405@163.com>
2025-03-01 14:44:22 +0800

    feat: 调整使用环境变量来定义数据库地址，向量化模型，AI_KEY，AI_BASE_URL，和model

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import { env } from "@/lib/env.mjs";
 
-const client = postgres('postgresql://postgres.egqxpvpblmaitfteftol:pIzSYE4bv7UvlV0y@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres');
+const client = postgres(env.DATABASE_URL);
 export const db = drizzle(client);
 
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from 'drizzle-kit';
+import { env } from '@/lib/env.mjs';
 
 export default {
-  schema: './db//schema.ts',
+  schema: './db/schema.ts',
   dialect: 'postgresql',
   out: './db/migrations',
   dbCredentials: {
-    url: 'postgresql://postgres.egqxpvpblmaitfteftol:pIzSYE4bv7UvlV0y@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres'
+    url: env.DATABASE_URL
   },
   tablesFilter: ['!langchain_embeddings', '!llamaindex_embeddings']
 } satisfies Config;
