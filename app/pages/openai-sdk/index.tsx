69f5e3c1c2304049a74847e7b09c317bf4fe554b
nafek.liu <nafek0405@163.com>
2025-03-03 15:05:43 +0800

    feat: 重新配置数据表

--- app/pages/openai-sdk/index.tsx
+++ app/pages/openai-sdk/index.tsx
@@ -1,7 +1,17 @@
 'use client';
 
+import ChatMessages from '@/app/components/ChatMessages/ChatMessages';
 const Home = () => {
-  return <div>Home</div>;
+  return <ChatMessages
+    messages={[]}
+    input={''}
+    handleInputChange={() => {}}
+    onSubmit={() => {}}
+    isLoading={false}
+    messageImgUrl={''}
+    setMessagesImgUrl={() => {}}
+    onRetry={() => {}}
+   />;
 };
 
 export default Home;
